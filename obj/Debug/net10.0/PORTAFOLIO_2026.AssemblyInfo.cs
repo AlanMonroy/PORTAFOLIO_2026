3f5f94003e3533a80b89e0a631b3cdc8becb6e38
Alan Monroy <alanmonroyzz99@gmail.com>
2026-05-01 14:32:15 -0600

    cambios perfil

--- obj/Debug/net10.0/PORTAFOLIO_2026.AssemblyInfo.cs
+++ obj/Debug/net10.0/PORTAFOLIO_2026.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PORTAFOLIO_2026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82796caaf876fdb1148a064d5a3b3e24be51ce4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2536945278d468c32241b67ce1491013a4f0d615")]
 [assembly: System.Reflection.AssemblyProductAttribute("PORTAFOLIO_2026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PORTAFOLIO_2026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
